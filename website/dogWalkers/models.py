dbc90a075671906ea68dd7bb258a2324528b88a3
aliKarim27 <99358134+aliKarim27@users.noreply.github.com>
2022-03-01 13:23:08 +0000

    Update models.py
    class for dog walkers profile, basic attributes added

--- website/dogWalkers/models.py
+++ website/dogWalkers/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class DogWalkerPost(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    distance = models.FloatField()
+    experience = models.IntegerField()
+    pricing = models.IntegerField()
+    bio = models.TextField()
+    rating = models.IntegerField()
+    additional_info =  models.TextField()
+    user_img = models.ImageField()
+
+    def __str__(self):
+        return self.name
