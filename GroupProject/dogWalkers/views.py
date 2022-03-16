b6604722ab5f77fed0ddfa3d7bd86831cfc5a5c1
Rafe <rafebennett22@gmail.com>
2022-03-16 10:51:20 +0000

    rolled back

--- GroupProject/dogWalkers/views.py
+++ GroupProject/dogWalkers/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-from django.utils import timezone
-from .models import Post
 
 # Create your views here.
-def home(request):
+def dogWalkersPage(request):
 	return render(request, 'dogWalkers/dogWalkersPage.html', {})
-
-def walker_list(request):
-    posts = Post.objects.filter(posted_date__lte=timezone.now()).order_by('posted_date')
-    return render(request, 'dogWalkers/dogWalkersPage.html', {'listings':listings})
--- GroupProject/login/models.py
+++ GroupProject/login/models.py
@@ -1,4 +1,3 @@
-#models file for homepage
-
-from django.conf import settings
 from django.db import models
+
+# Create your models here.
