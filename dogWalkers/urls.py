e84f3fea628fff1248752b4f05e6e58cae1a7b60
Stephen Rollinson <stephen.rollinson.jla@gmail.com>
2022-03-22 17:48:35 +0000

    Creating links from dogWalkers to Homepage.

--- dogWalkers/urls.py
+++ dogWalkers/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.dogWalkers, name='dogWalkers'),
+	path('..\..\..\homepage\home.html', views.home, name='home'),
+	path('..\..\..\dogTrainers\index.html', views.dogTrainers, name='dogTrainers'),
 ]
--- homepage/urls.py
+++ homepage/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name='home'),
-	path('..\..\..\dogWalkers\dogWalkersPage.html', views.home, name='dogWalkers'),
-	path('..\..\..\dogTrainers\index.html', views.home, name='dogTrainers'),
+	path('..\..\..\dogWalkers\dogWalkersPage.html', views.dogWalkers, name='dogWalkers'),
+	path('..\..\..\dogTrainers\index.html', views.dogTrainers, name='dogTrainers'),
 ]
