08c4521b1186dc72819d315a2332abbd80460e98
Rafe <rafebennett22@gmail.com>
2022-03-23 11:05:23 +0000

    connected home and walkers using django urls

--- dogWalkers/urls.py
+++ dogWalkers/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.dogWalkers, name='dogWalkers'),
-	path('..\..\..\homepage\home.html', views.dogWalkers, name='home'),
-	path('..\..\..\dogTrainers\index.html', views.dogWalkers, name='dogTrainers'),
+	#path('..\..\..\homepage\home.html', views.dogWalkers, name='home'),
+	#path('..\..\..\dogTrainers\index.html', views.dogWalkers, name='dogTrainers'),
+	path('home/', views.home, name='home'),
 ]
--- dogWalkers/views.py
+++ dogWalkers/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def dogWalkers(request):
 	return render(request, 'dogWalkers/dogWalkersPage.html', {})
+	
+def home(request):
+	return render(request, 'homepage/home.html', {})
