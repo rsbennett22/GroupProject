ebd34e5378dfe6c56004010b3b8bf412b59e8496
Rafe Bennett <71774949+rsbennett22@users.noreply.github.com>
2022-03-23 22:25:15 +0000

    Revert "hyperlinks established between all currently working pages and the fo…"

--- dogWalkers/views.py
+++ dogWalkers/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 from .models import DogWalker
 
 # Create your views here.
+def dogWalkers(request):
+	#initialy order the users by their price
+	walkers = DogWalker.objects.order_by('price')
+	return render(request, 'dogWalkers/dogWalkersPage.html', {'walkers':walkers})
+	
 def home(request):
 	return render(request, 'homepage/home.html', {})
-	
-def dogWalkers(request):
-	return render(request, 'dogWalkers/dogWalkersPage.html', {})
-
-def dogTrainers(request):
-	return render(request, 'dogTrainers/index.html', {})
