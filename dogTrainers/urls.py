37e3b70e5a18c75779c5c53db21c3e6c375cf626
Stephen Rollinson <stephen.rollinson.jla@gmail.com>
2022-03-23 17:49:28 +0000

    hyperlinks established between all currently working pages and the fod trainers page.

---
+++ dogTrainers/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('', views.home, name='home'),
+	path('dogWalkers', views.dogWalkers, name="dogWalkers"),
+	path('dogTrainers', views.dogTrainers, name="dogTrainers"),
+]
