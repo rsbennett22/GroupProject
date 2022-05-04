be876adefc53376ad614ad1a3427a50f42803bba
xian-yu626 <63033890+xian-yu626@users.noreply.github.com>
2022-05-04 16:55:12 +0800

    Update serializers.py

--- dogTraining/serializers.py
+++ dogTraining/serializers.py
@@ -4,4 +4,4 @@ from .models import DogTraining
 class DogTrainingSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = DogTraining
-		fields = ('name', 'email', 'postcode', 'price', 'rating','usr_info', 'avbl_from', 'avbl_to', 'acpt_7k', 'acpt_18k', 'acpt_45k', 'acpt_abv_45k', 'acpt_Trainer')
+		fields = ('name', 'email', 'postcode', 'price', 'rating','usr_info', 'avbl_from', 'avbl_to', 'acpt_7k', 'acpt_18k', 'acpt_45k', 'acpt_abv_45k', 'acpt_Trainer', 'acpt_style')
