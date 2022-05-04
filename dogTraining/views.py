d36134c748088007ea9bff6209c1b14d4914f20f
xian-yu626 <63033890+xian-yu626@users.noreply.github.com>
2022-05-04 16:54:27 +0800

    Update views.py

--- dogTraining/views.py
+++ dogTraining/views.py
@@ -10,61 +10,132 @@ from django.conf import settings
 from django.http import HttpResponse
 import logging
 import os
+# import checkPrice from DogTraining.js
 
 @api_view(['GET', 'POST', 'DELETE'])
 def dogTraining_list(request):
 
-	#get all the dogTraining
-	if request.method == 'GET':
-		dogTraining = DogTraining.objects.all()
-		dogTraining_serializer = DogTrainingSerializer(dogTraining, many=True)
-		return JsonResponse(dogTraining_serializer.data, safe=False)
+    #get all the dogTraining
+    if request.method == 'GET':
+        dogTraining = DogTraining.objects.all()
+        dogTraining_serializer = DogTrainingSerializer(dogTraining, many=True)
+        return JsonResponse(dogTraining_serializer.data, safe=False)
 
-	#create a new dogTrainer
-	elif request.method == 'POST':
-		dogTraining_data = JSONParser().parse(request)
-		dogTraining_serializer = DogTrainingSerializer(data = dogTraining_data)
-		if dogTraining_serializer.is_valid():
-			dogTraining_serializer.save()
-			return JsonResponse(dogTraining_serializer.data, status=status.HTTP_201_CREATED)
+    #create a new dogTraining
+    elif request.method == 'POST':
+        dogTraining_data = JSONParser().parse(request)
+        dogTraining_serializer = DogTrainingSerializer(data = dogTraining_data)
+        if dogTraining_serializer.is_valid():
+            dogTraining_serializer.save()
+            return JsonResponse(dogTraining_serializer.data, status=status.HTTP_201_CREATED)
 
-	#delete all dogTrainer
-	elif  request.method == 'DELETE':
-		count = DogTraining.objects.all().delete()
-		return JsonResponse({'message':'{} DogTraining were deleted successfully!'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
+    #delete all dogTraining
+    elif  request.method == 'DELETE':
+        count = DogTraining.objects.all().delete()
+        return JsonResponse({'message':'{} DogTrainer were deleted successfully!'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def dogTraining_detail(request, pk):
-	#find dogTrainer by primary key (pk), the id
-	try:
-		dogTraining = DogTraining.objects.get(pk=pk)
-	except DogTraining.DoesNotExist:
-		return JsonResponse({'message': 'DogTrainer does not exist!'}, status=status.HTTP_404_NOT_FOUND)
+    #find dogTraining by primary key (pk), the id
+    try:
+        dogTraining = DogTraining.objects.get(pk=pk)
+    except DogTraining.DoesNotExist:
+        return JsonResponse({'message': 'DogTrainer does not exist!'}, status=status.HTTP_404_NOT_FOUND)
 
-	#retrieve a single dogTrainer
-	if request.method == 'GET':
-		dogTraining_serializer = DogTrainingSerializer(dogTraining)
-		return JsonResponse(dogTraining_serializer.data)
+    #retrieve a single dogTraining
+    if request.method == 'GET':
+        dogTraining_serializer = DogTrainingSerializer(dogTraining)
+        return JsonResponse(dogTraining_serializer.data)
 
-	#update a dogTrainer
-	elif request.method == 'PUT':
-		dogTraining_data = JSONParser().parse(request)
-		dogTraining_serializer = DogTrainingSerializer(dogTraining, data=dogTraining_data)
-		if dogTraining_serializer.is_valid():
-			dogTraining_serializer.save()
-			return JsonResponse(dogTraining_serializer.data)
-		return JsonResponse(dogTraining_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #update a dogTraining
+    elif request.method == 'PUT':
+        dogTraining_data = JSONParser().parse(request)
+        dogTraining_serializer = DogTrainingSerializer(dogTraining, data=dogTraining_data)
+        if dogTraining_serializer.is_valid():
+            dogTraining_serializer.save()
+            return JsonResponse(dogTraining_serializer.data)
+        return JsonResponse(dogTraining_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-	#delete a single dogTrainer
-	elif request.method == 'DELETE':
-		dogTraining.delete()
-		return JsonResponse({'message': 'DogTrainer successfully deleted!'}, status=status.HTTP_204_NO_CONTENT)
+    #delete a single dogTraining
+    elif request.method == 'DELETE':
+        dogTraining.delete()
+        return JsonResponse({'message': 'DogTrainer successfully deleted!'}, status=status.HTTP_204_NO_CONTENT)
+
 
 @api_view(['GET'])
-def dogTraining_acpt_pup(request):
-	dogTraining = DogTraining.objects.filter(acpt_pup=True)
+def dogTraining_acpt_Trainer(request):
+    dogTraining = DogTraining.objects.filter(dogTraining_acpt_Trainer=True) 
+    if request.method == 'GET':
+        dogTraining_serializer = DogTrainingSerializer(dogTraining, many=True)
+        return JsonResponse(dogTraining_serializer.data, safe=False)
 
-	#get a dogTrainer that accept puppies
-	if request.method == 'GET':
-		dogTraining_serializer = DogTrainingSerializer(dogTraining, many=True)
-		return JsonResponse(dogTraining_serializer.data, safe=False)
+@api_view(['GET'])
+def dogTraining_acpt_style(request):
+    dogTraining = DogTraining.objects.filter(dogTraining_acpt_style=True) 
+    if request.method == 'GET':
+        dogTraining_serializer = DogTrainingSerializer(dogTraining, many=True)
+        return JsonResponse(dogTraining_serializer.data, safe=False)
+
+
+@api_view(['GET'])
+def dogTraining_acpt_7k(request):
+    dogTraining = DogTraining.objects.filter(acpt_7k=True) 
+
+    #get a dogTraining that accept dogs below 7kg 
+    if request.method == 'GET':
+        dogTraining_serializer = DogTrainingSerializer(dogTraining, many=True)
+        return JsonResponse(dogTraining_serializer.data, safe=False)
+
+@api_view(['GET'])
+def dogTraining_acpt_18k(request):
+    dogTraining = DogTraining.objects.filter(acpt_18k=True) 
+
+    #get a dogTraining that accept dogs below 18kg
+    if request.method == 'GET':
+        dogTraining_serializer = DogTrainingSerializer(dogTraining, many=True)
+        return JsonResponse(dogTraining_serializer.data, safe=False)
+
+@api_view(['GET'])
+def dogTraining_acpt_45k(request):
+    dogTraining = DogTraining.objects.filter(acpt_45k=True) 
+
+    #get a dogTraining that accept accept dogs below 45kg
+    if request.method == 'GET':
+        dogTraining_serializer = DogTrainingSerializer(dogTraining, many=True)
+        return JsonResponse(dogTraining_serializer.data, safe=False)
+
+@api_view(['GET'])
+def dogTraining_acpt_abv_45k(request):
+    dogTraining = DogTraining.objects.filter(acpt_abv_45k=True) 
+
+    #get a dogTraining that accept dogs above 45k 
+    if request.method == 'GET':
+        dogTraining_serializer = DogTrainingSerializer(dogTraining, many=True)
+        return JsonResponse(dogTraining_serializer.data, safe=False)
+
+
+
+@api_view(['GET'])
+def dogTraining_price(request):
+    if request.method == "GET":
+        dogTraining=DogTraining.objects.all()
+        value = request.query_params.get('price', None)
+        if value is not None:
+            dogTraining=dogTraining.filter(price__lte=value)
+        dogTraining_serializer = DogTrainingSerializer(dogTraining, many=True)
+        return JsonResponse(dogTraining_serializer.data, safe=False)
+    #This method gets the slider value from the url (line 152) and then uses this in the filtering logic
+
+
+
+@api_view(['GET'])
+def dogTraining_date_range(request):
+    if request.method == "GET":
+        dogTraining=DogTraining.objects.all()
+        startDate = request.query_params.get('avbl_from', None)
+        endDate = request.query_params.get('avbl_to', None)
+        if startDate and endDate is not None:
+            dogTraining=(dogTraining.filter(avbl_from__gte=startDate, avbl_from__lte=endDate))or (dogTraining.filter(avbl_to__gte=startDate, avbl_to__lte=endDate))or (dogTraining.filter(avbl_from__lte=startDate, avbl_to__gte=endDate)) 
+        dogTraining_serializer = DogTrainingSerializer(dogTraining, many=True)
+        return JsonResponse(dogTraining_serializer.data, safe=False)
+    #This method gets the start and end date selected from the datepicker from the url (line 165,166) and then uses this in the filtering logic
