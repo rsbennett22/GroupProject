da38a83640441277a53e32725c3f7249741c8268
xian-yu626 <63033890+xian-yu626@users.noreply.github.com>
2022-05-12 23:17:55 +0800

    Update apps.py

--- dogTraining/apps.py
+++ dogTraining/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DogWalkersConfig(AppConfig):
+class DogTrainingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dogTraining'
